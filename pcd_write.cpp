773928884878416fdf1a3c4f6d013183b7e1ae00
zuqing xie <xiezuqing2015@gmail.com>
2022-11-30 12:56:41 +0100

    commit learning

---
+++ pcd_write.cpp
@@ -0,0 +1,30 @@
+#include <pcl/io/pcd_io.h>
+#include <iostream>
+#include <pcl/point_types.h>
+
+int
+  main ()
+{
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+
+  // Fill in the cloud data
+  cloud.width    = 5;
+  cloud.height   = 100;
+  cloud.is_dense = false; // meigh be infinit or NaN.
+  cloud.resize (cloud.width * cloud.height);
+
+  for (auto& point: cloud)
+  {
+    point.x = 1024 * rand () / (RAND_MAX + 1.0f);
+    point.y = 1024 * rand () / (RAND_MAX + 1.0f);
+    point.z = 1024 * rand () / (RAND_MAX + 1.0f);
+  }
+
+  pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
+  std::cerr << "Saved " << cloud.size () << " data points to test_pcd.pcd." << std::endl;
+
+  for (const auto& point: cloud)
+    std::cerr << "    " << point.x << " " << point.y << " " << point.z << std::endl;
+
+  return (0);
+}
---
+++ pfh.cpp
@@ -0,0 +1,1 @@
+#include<pcl/features>
